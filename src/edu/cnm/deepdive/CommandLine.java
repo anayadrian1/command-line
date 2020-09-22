2dd8fa2184accdc053b9bba607a8bacee74b1415
Adrian Anaya <aanaya79@cnm.edu>
2020-09-22 09:53:30 -0600

    Comment change

--- src/edu/cnm/deepdive/CommandLine.java
+++ src/edu/cnm/deepdive/CommandLine.java
@@ -3,6 +3,7 @@ package edu.cnm.deepdive;
 public class CommandLine {
 
   public static void main(String[] args){
+    //Enhanced for loops are less error prone
     for (String current : args) {
       System.out.println(current.toUpperCase());
     }
